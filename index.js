4c653e6f398f61400c5f303cc55108282b60e411
IonitaCatalin <ionita.catalin2000@gmail.com>
2019-03-17 20:11:36 +0200

    index.js for about section

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ var app = express();
 var http = require('http').Server(app);
 var port = process.env.PORT || 3000;
 
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-app.use(express.static(__dirname + '/public'));
+app.get('/about.html',function(req,res){
+  res.sendFile(__dirname+'/about.html');
+});
 
 http.listen(port, function(){
   console.log('listening on *:' + port);
 });
+
